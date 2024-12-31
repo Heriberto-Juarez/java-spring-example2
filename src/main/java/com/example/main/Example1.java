4c69ebeac7f5ffdf7cbf6a49022d42d0c7706e82
Heriberto-Juarez <heribertojuarezj@gmail.com>
2024-12-31 15:35:30 -0600

    remove manual instances creation and get bean by name

--- src/main/java/com/example/main/Example1.java
+++ src/main/java/com/example/main/Example1.java
@@ -6,11 +6,8 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class Example1 {
     public static void main(String[] args) {
-        Vehicle vehicle = new Vehicle();
-        vehicle.setName("Lamborghini Veneno");
-        System.out.println("Vehicle name form non-spring context is: " + vehicle.getName());
         var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
-        Vehicle veh = context.getBean(Vehicle.class);
+        Vehicle veh = context.getBean("vehicle3",Vehicle.class);
         System.out.println("Vehicle from Spring Context is: " + veh.getName());
     }
 }
