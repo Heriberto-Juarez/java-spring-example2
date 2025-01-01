4768e02bce791c342659dd89c9cfd4ebc809e511
Heriberto-Juarez <heribertojuarezj@gmail.com>
2025-01-01 08:54:54 -0600

    perf: remove unused import

--- src/main/java/com/example/main/Example2.java
+++ src/main/java/com/example/main/Example2.java
@@ -5,7 +5,6 @@ import com.example.config.ProjectConfig;
 import org.springframework.beans.factory.NoSuchBeanDefinitionException;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-import javax.swing.*;
 import java.util.Random;
 import java.util.function.Supplier;
 
