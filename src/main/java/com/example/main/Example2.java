0af57d10de135d7cd61cac7ba560cebd9363cede
Heriberto-Juarez <heribertojuarezj@gmail.com>
2025-01-01 09:12:39 -0600

    chore: example of how to use xml for annotaitons (not recommended)

--- src/main/java/com/example/main/Example2.java
+++ src/main/java/com/example/main/Example2.java
@@ -1,12 +1,11 @@
 package com.example.main;
 
 import com.example.beans.Vehicle;
-import com.example.config.ProjectConfig;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Example2 {
     public static void main(String[] args) {
-        var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+        var context = new ClassPathXmlApplicationContext("beans.xml");
 
         Vehicle primaryVehicle = context.getBean(Vehicle.class);
         System.out.println("Vehicle is: " + primaryVehicle.getName());
